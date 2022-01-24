ed80287b88d3a96b05afbd17763f52678a106b9a
Jake Crowley <jakecrow@gmail.com>
2022-01-23 20:08:28 -0500

    fix ape spelling :skull:

--- Sortify/lib/landing/landing.dart
+++ Sortify/lib/landing/landing.dart
@@ -655,7 +655,8 @@ class _DesktopLanding extends State<DesktopLanding> {
     switch (selected) {
       // Sign in Button
       case 0:
-        child = Container(
+        child = MouseRegion(
+            cursor: SystemMouseCursors.click,
             child: Tooltip(
                 // Tooltip message
                 message: 'Link your Spotify Account',
